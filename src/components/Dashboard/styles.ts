35d7c25c87bb7b272e4dc5780b08b2bb21e56ae8
ellenassis <ellenassis8@gmail.com>
2022-06-23 16:10:23 -0300

    feat: add Dashboard styles

--- src/components/Dashboard/styles.ts
+++ src/components/Dashboard/styles.ts
@@ -1,3 +1,7 @@
 import styled from "styled-components";
 
-export const Container = styled.main ``;
+export const Container = styled.main `
+    margin: 0 auto;
+    max-width: 1120px;
+    padding: 0 1rem;
+`;
