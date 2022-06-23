b74fa655763cc6036bce2f761e4fe46517b0681d
ellenassis <ellenassis8@gmail.com>
2022-06-23 15:48:06 -0300

    att

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import styled from 'styled-components'
+import { Header } from './components/Header';
+import { Dashboard } from './components/Dashboard';
+import { GlobalStyle } from './styles/global';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+      <>
+      <Header />
+      <Dashboard />
+      <GlobalStyle />
+      </>
   );
 }
 
