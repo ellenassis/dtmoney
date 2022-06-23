36432383f378e0c13acd1acc90a3229f0c5d7073
ellenassis <ellenassis8@gmail.com>
2022-06-23 16:09:48 -0300

    feat: add green root

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,7 +5,7 @@ export const GlobalStyle = createGlobalStyle`
         --background: #f8f2f5;
         --red: #e52e4d;
         --blue: #5429CC;
-
+        --green: #33CC95;
         --blue-light: #6933ff;
 
         --text-title: #363f5f;
