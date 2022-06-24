abd960476b965c2109abe52e5709534b06fc35e2
ellenassis <ellenassis8@gmail.com>
2022-06-24 17:26:16 -0300

    feat: add modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components'
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
 import { GlobalStyle } from './styles/global';
+import { TransactionModal } from './components/TransactionModal';
+
+import Modal from "react-modal";
+
+Modal.setAppElement('#root');
 
 function App() {
+  const [isTransactionModalOpen, setTransactionModalOpen] = useState(false);
+
+  const handleOpenTransactionModal = () => {
+    setTransactionModalOpen(true);
+  }
+
+  const handleCloseTransactionModal = () => {
+    setTransactionModalOpen(false);
+  }
+  
   return (
       <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenTransactionModal}/>
       <Dashboard />
+
       <GlobalStyle />
+      <TransactionModal isOpen={isTransactionModalOpen} onRequestClose={handleCloseTransactionModal} />
       </>
   );
 }
