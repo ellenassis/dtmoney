e2dababb43f6d473e4ea3ff7dc8de01c77c0d87a
ellenassis <ellenassis8@gmail.com>
2022-06-24 17:25:28 -0300

    feat: add transaction component

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Summary } from '../Summary'
+import { Transactions } from '../Transactions'
 import { Container } from './styles'
 
 export function Dashboard() {
   return (
     <Container>
         <Summary />
+        <Transactions />
     </Container>
   )
 }
