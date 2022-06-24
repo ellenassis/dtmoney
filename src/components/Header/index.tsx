d7f07b7bf4535fd2a8ca8cbea7800d5c4fe0cf13
ellenassis <ellenassis8@gmail.com>
2022-06-24 17:26:37 -0300

    feat: add onclick on button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import { Container, Content } from './styles';
-import logoImg from '../../assets/logo.svg';
+import React from "react";
+import { Container, Content } from "./styles";
+import logoImg from "../../assets/logo.svg";
 
-export function Header() {
-    return (
-        <Container>
-            <Content>
-                <img src={logoImg} alt="dt money" />
-                <button type='button'>
-                    Nova transação
-                </button>
-            </Content>
-        </Container>
-    )
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+
+export function Header({onOpenNewTransactionModal}: HeaderProps) {
+   
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="dt money"/>
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </button>
+      </Content>
+
+    </Container>
+  );
 }
