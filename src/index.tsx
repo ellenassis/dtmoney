0a0e4c911260ebbf1e10902100cf62c968365df2
ellenassis <ellenassis8@gmail.com>
2022-06-26 15:20:42 -0300

    fix: change values

--- src/index.tsx
+++ src/index.tsx
@@ -16,7 +16,7 @@ createServer({
           title: 'Freelancer de website',
           type: 'deposit',
           category: 'Dev',
-          amount: 6000,
+          amount: 5000,
           createdAt: new Date('2021-02-12 09:00:00'),
         },
         {
@@ -24,7 +24,7 @@ createServer({
           title: 'Aluguel',
           type: 'withdraw',
           category: 'Casa',
-          amount: 1100,
+          amount: 1000,
           createdAt: new Date('2021-02-15 09:00:00'),
         },
       ],
